853dc1147a626f444302e0ce85abbc8d95f2f38d
Gorjan <gorjanz@gmail.com>
2018-09-05 22:48:09 +0200

    Added filter, for now just adding a header to make sure it went through it

---
+++ src/main/java/edu/gorjanz/requestcounter/configuration/RequestCounterFilter.java
@@ -0,0 +1,30 @@
+package edu.gorjanz.requestcounter.configuration;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class RequestCounterFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Checked-by-request-counter", String.valueOf(true));
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
